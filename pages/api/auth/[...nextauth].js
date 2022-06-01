58d256b1b89309bcbc0ecf6fe81d85d7cbe3d5e4
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-01 11:56:37 -0500

    Agrega la configuracion de MongoDBAdapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GithubProvider from "next-auth/providers/github";
+import clientPromise from "lib/mongodb";
 
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
